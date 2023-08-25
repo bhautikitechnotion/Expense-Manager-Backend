c9f7d704bf449daed4d5faf35877c626c909dfa8
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-08-25 19:06:10 +0530

    setted up express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-function addition(a: number, b: number) {
-    return a + b;
+import express, { Application } from 'express';
+import { envSettings } from '@src/utils/env.config';
+import cors from 'cors';
+import helmet from 'helmet';
+
+if (!envSettings.serverPort) {
+    process.exit(1);
 }
 
-console.log(addition(1565, 2));
+const app: Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(envSettings.serverPort, () => {
+    console.log(`Connecting to server at port ${envSettings.serverPort}`)
+})
