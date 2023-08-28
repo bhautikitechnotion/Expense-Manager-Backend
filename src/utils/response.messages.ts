8365675f0297c0b11a3a7020959b392687e638e7
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-08-28 17:14:26 +0530

    added response message file

---
+++ src/utils/response.messages.ts
@@ -0,0 +1,5 @@
+export const resMsg = {
+    SOMETHING_WENT_WRONG: 'Something went wrong!',
+    ALREADY_USER_REGISTERED: 'User already registered with this email',
+    USER_REGISTRATION_SUCCESSFUL: 'User registration successful',
+};
