678a039b4388207a3cee64b2b9a3e94db568f6b5
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-08-28 17:14:00 +0530

    created database connection

---
+++ src/connections/index.ts
@@ -0,0 +1,20 @@
+import { db, uri } from '@src/utils';
+import { Db, MongoClient, MongoClientOptions, MongoServerError } from 'mongodb';
+import { ConnectToCollections } from './connectToCollections';
+
+const dbClient = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+} as MongoClientOptions );
+
+export async function connectToDb(): Promise<void> {
+    try {
+        await dbClient.connect();
+
+        const dataBase: Db = dbClient.db(db);
+
+        await ConnectToCollections(dataBase)
+    } catch (error) {
+        console.log((error as MongoServerError).message);
+    }
+}
--- src/utils/env.config.ts
+++ src/utils/env.config.ts
@@ -3,10 +3,14 @@ dotenv.config({ path: '.env' });
 
 interface Config {
     serverPort: string | undefined;
+    uri: string | undefined;
+    db: string | undefined;
 }
 
 const envSettings: Config = {
     serverPort: process.env.SERVER_PORT,
+    uri: process.env.MONGO_CONNECTION_URL,
+    db: process.env.MONGO_DB,
 };
 
 export { envSettings }
