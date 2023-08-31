394925f3da386063ba18c2124d6181a26a95ced7
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-08-31 09:40:36 +0530

    category related database changes

--- src/connections/collections.name.ts
+++ src/connections/collections.name.ts
@@ -1,1 +1,3 @@
 export const USERS = '01a_users';
+
+export const CATEGORIES = '02_categories';
--- src/connections/connections.ts
+++ src/connections/connections.ts
@@ -2,4 +2,5 @@ import { Collection } from 'mongodb';
 
 export const collections: {
     userCollection?: Collection;
+    categoryCollection?: Collection;
 } = {};
