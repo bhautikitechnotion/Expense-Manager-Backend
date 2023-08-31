93829adf155ddf3c9cbc0063505dc6982bf0c17b
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-08-31 09:26:45 +0530

    created category router and related setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { connectToDb } from './connections';
 import userRouter from './services/user/routes';
+import categoryRouter from './services/category/routes';
 
 if (!envSettings.serverPort) {
     process.exit(1);
@@ -17,6 +18,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/user', userRouter)
+app.use('/category', categoryRouter)
 
 app.listen(envSettings.serverPort, async () => {
     console.log(`Connecting to server at port ${envSettings.serverPort}`)
