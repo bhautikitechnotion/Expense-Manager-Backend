3e489dcbe3d0b95dba75b3929ceb2d7a6150f1cf
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-09-01 11:08:50 +0530

    added USERS_TYPES collection in DB connection

--- src/connections/connectToCollections.ts
+++ src/connections/connectToCollections.ts
@@ -1,15 +1,17 @@
 import { Collection, Db } from 'mongodb';
-import { CATEGORIES, EXPENSES, PAYMENTS, SUB_CATEGORIES, USERS } from './collections.name';
+import { CATEGORIES, EXPENSES, PAYMENTS, SUB_CATEGORIES, USERS, USERS_TYPES } from './collections.name';
 import { collections } from './connections';
 
 export const ConnectToCollections = async (dataBase: Db) => {
     const userCollection: Collection = dataBase.collection(USERS);
+    const usersTypesCollection: Collection = dataBase.collection(USERS_TYPES);
     const categoryCollection: Collection = dataBase.collection(CATEGORIES);
     const subCategoryCollection: Collection = dataBase.collection(SUB_CATEGORIES);
     const expensesCollection: Collection = dataBase.collection(EXPENSES);
     const paymentsCollection: Collection = dataBase.collection(PAYMENTS);
 
     collections.userCollection = userCollection;
+    collections.usersTypesCollection = usersTypesCollection;
     collections.categoryCollection = categoryCollection;
     collections.subCategoryCollection = subCategoryCollection;
     collections.expensesCollection = expensesCollection;
--- src/connections/connections.ts
+++ src/connections/connections.ts
@@ -2,6 +2,7 @@ import { Collection } from 'mongodb';
 
 export const collections: {
     userCollection?: Collection;
+    usersTypesCollection?: Collection;
     categoryCollection?: Collection;
     subCategoryCollection?: Collection;
     expensesCollection?: Collection;
