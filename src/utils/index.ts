5d348e76e29a677389659adbfce518843b738899
Bhautik Gajera <bhautik.gajera@itechnotion.in>
2023-09-01 14:12:38 +0530

    added decryptToken util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb';
 import { envSettings } from './env.config';
 import bcrypt from 'bcrypt';
 import jwt, { JwtPayload, Secret, SignOptions, TokenExpiredError, VerifyCallback } from 'jsonwebtoken';
+import { isDate } from 'lodash';
 
 export const isValidObject = (obj: any): boolean => {
     return obj && Object.keys(obj).length > 0;
@@ -75,7 +76,7 @@ interface Token {
 
 export const encryptToken = (token: string, expires: string | number = '1d'): Token => {
     try {
-        const hash = jwt.sign({ token }, userTokenSecretKey as Secret, { expiresIn: expires, issuer: 'adtendees' } as SignOptions);
+        const hash = jwt.sign({ token }, userTokenSecretKey as Secret, { expiresIn: expires, issuer: 'expenseManager' } as SignOptions);
         return {
             hashToken: hash,
             success: true,
@@ -88,37 +89,31 @@ export const encryptToken = (token: string, expires: string | number = '1d'): To
     }
 }
 
-// export const decryptToken = (hashToken: string, options: { userId: string }): Token => {
-//     try {
-//         const decode = jwt.verify(hashToken, userTokenSecretKey as Secret) as VerifyCallback<JwtPayload>;
-//         return {
-//             decodeToken: decode,
-//             success: true,
-//             isExpiredToken: false
-//         };
-//     } catch (error) {
-//         const { expiredAt, message, name, inner } = (error as TokenExpiredError);
-//         let isExpiredToken: boolean | undefined = false;
+export const decryptToken = (hashToken: string): Token => {
+    try {
+        const decode = jwt.verify(hashToken, userTokenSecretKey as Secret) as VerifyCallback<JwtPayload>;
+        return {
+            decodeToken: decode,
+            success: true,
+            isExpiredToken: false
+        };
+    } catch (error) {
+        const { expiredAt, message, name, inner } = (error as TokenExpiredError);
 
-//         // if token expired
-//         if (isDate(expiredAt)) {
-//             isExpiredToken = true;
+        let isExpiredToken: boolean | undefined = false;
 
-//             const { userId } = options;
+        // if token expired
+        if (isDate(expiredAt)) {
+            isExpiredToken = true;
+        }
 
-//             // remove active device -1, sign out_time,if hashToken avail then removed
-//             join(updateUserLogoutByTokenExpired({ userId: userId, token: hashToken }), (res) => {
-//                 const { update, data } = res;
-//             });
-//         }
-
-//         return {
-//             token: null,
-//             success: false,
-//             isExpiredToken
-//         }
-//     }
-// }
+        return {
+            token: null,
+            success: false,
+            isExpiredToken
+        }
+    }
+}
 
 export const isValidMongoId = (id: string) => {
     const checkMongoId = new RegExp("^[0-9a-fA-F]{24}$");
